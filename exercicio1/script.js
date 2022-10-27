f869e9d38acb8cfb3d34c47074ecbea5fba6c9dc
Fabio-kato <fabiobortoluci@gmail.com>
2022-10-27 18:06:42 -0300

    exercício 1 array

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const numeros = [4, 6, 2, 5, 8]
+
+const strings = ['fabio']
+
+const mista = [5, 'bete', true]
+
+console.log('numeros', numeros)
+console.log('strings', strings)
+console.log('mista', mista)
